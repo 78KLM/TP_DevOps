aa6aab868db0b04c90034f66b2d859c41a877e8c
78KLM <kalembaexoce@gmail.com>
2026-03-18 13:21:31 +0100

    test: add invalid cases and exception handling for Panier

--- test/PanierTest.java
+++ test/PanierTest.java
@@ -52,4 +52,36 @@ class PanierTest {
         // Affirmer
         assertFalse(panier.estVide());
     }
+
+    @Test
+    void quantiteNulleDoitLeverUneException() {
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo", 1.50);
+        assertThrows(IllegalArgumentException.class, () -> panier.ajouterArticle(article, 0));
+    }
+
+    @Test
+    void articleNulDoitLeverException() {
+        Panier panier = new Panier();
+        assertThrows(IllegalArgumentException.class, () -> panier.ajouterArticle(null, 1));
+    }
+
+    @Test
+    void quantiteNegativeDoitLeverException() {
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo", 1.50);
+        assertThrows(IllegalArgumentException.class, () -> panier.ajouterArticle(article, -3));
+    }
+
+    @Test
+    void codeReductionVideDoitLeverException() {
+        Panier panier = new Panier();
+        assertThrows(IllegalArgumentException.class, () -> panier.appliquerCodeReduction(""));
+    }
+
+    @Test
+    void codeReductionNulDoitLeverException() {
+        Panier panier = new Panier();
+        assertThrows(IllegalArgumentException.class, () -> panier.appliquerCodeReduction(null));
+    }
 }
