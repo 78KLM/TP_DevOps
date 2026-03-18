03163e06230902d4b762538cb14ab83c0f98cca4
78KLM <kalembaexoce@gmail.com>
2026-03-18 13:11:02 +0100

    test: add initial valid cases for Panier

---
+++ test/PanierTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PanierTest {
+
+    @Test
+    void ajouterArticleDoitAugmenterLeNombreDeArticles() {
+        // Arranger
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo bleu", 1.50);
+
+        // Agir
+        panier.ajouterArticle(article, 2);
+
+        // Affirmer
+        assertEquals(1, panier.nombreArticles());
+    }
+
+    @Test
+    void calculerTotalDoitRetournerLaSommeDesSousTotaux() {
+        // Arranger
+        Panier panier = new Panier();
+        Article article1 = new Article("REF-001", "Stylo bleu", 1.50);
+        Article article2 = new Article("REF-002", "Stylo rouge", 1.50);
+
+        // Agir
+        panier.ajouterArticle(article1, 3);
+        panier.ajouterArticle(article2, 3);
+
+        // Affirmer
+        assertEquals(9.00, panier.calculerTotal(), 0.001);
+    }
+
+    @Test
+    void panierVideDoitRetournerEstVideEgalTrue() {
+        // Arranger
+        Panier panier = new Panier();
+
+        // Agir & Affirmer
+        assertTrue(panier.estVide());
+    }
+
+    @Test
+    void panierAvecArticlesNeDoitPasEtreVide() {
+        // Arranger
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo bleu", 1.50);
+
+        // Agir
+        panier.ajouterArticle(article, 1);
+
+        // Affirmer
+        assertFalse(panier.estVide());
+    }
+}
